544ffd249a4dfec74fae327046308eb0c55f101f
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2023-04-15 15:44:54 +0200

    Tested on bbb

--- examples/eh_debug.py
+++ examples/eh_debug.py
@@ -1,6 +1,7 @@
 import eco_hectare as eh
 import random
 import datetime
+import time
 
 # --- Sets up data base ---
 db = eh.db.DataBase(db_file='../eco_hectare/static/main.db', create=True)
@@ -14,5 +15,22 @@ def insert_random_measurements(deveui, n):
     for t, m in zip(ts, meas):
         db.insert_measurement(deveui, m, t)
 
+def getTemp():
+    file = '/sys/class/hwmon/hwmon0/temp1_input'
+    with open(file, 'r') as f:
+        data = f.read()
     
+    return float(data) / 1000.0
+
+#insert_random_measurements('75775f04d7f01fe0', 10)
+#insert_random_measurements('3dc395241c7f8501', 10)
+
+while True:
     
+    t = datetime.datetime.today().isoformat(' ', 'seconds')
+    temp = getTemp()
+    
+    print('Temperature: {:.2f}\tTime stamp: {:}'.format(temp, t))
+    db.insert_measurement('75775f04d7f01fe0', temp, t)
+    
+    time.sleep(60)
